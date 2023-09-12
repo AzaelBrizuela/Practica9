4f7b1f0ad60faf87adbb4ae651edf7921be4e653
Azael Brizuela <azaelbrizuela564@gmail.com>
2023-09-12 17:39:51 -0600

    alta 1

--- EntidadPractica9/Productos.cs
+++ EntidadPractica9/Productos.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace EntidadPractica9
 {
-    public class Class1
+    public class Productos
     {
+        private int _id_producto;
+        private string _nombre;
+        private string _descripcion;
+        private double _precio;
+
     }
 }
